2c122c3a08cebf5f2bf6fc747c888a2ae47c7ed2
Ben <71541167+BenVN123@users.noreply.github.com>
2021-03-06 02:02:45 -0800

    Delete site directory

--- site/views.py
+++
@@ -1,9 +0,0 @@
-import functools
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-from Site.db import get_db
-
-views = Blueprint('views', __name__, url_prefix='/')
-
-@views.route('about')
-def about():
-    render_template('')
